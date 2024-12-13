65d1deaa84d5d234c36b5f2c3100c0ad846c6887
Natali280853 <dgorby2000@gmail.com>
2024-12-13 21:11:19 +0500

    add templates

--- mysite/mymain/urls.py
+++ mysite/mymain/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
 from . import views
 
+# urlpatterns = [
+#     path('', views.index),
+#     path('new', views.new)
+#    ]
+
 urlpatterns = [
-    path('', views.index),
-    path('new', views.new)
-   ]
+    path('', views.page1, name='page1'),
+    path('page2/', views.page2, name='page2'),
+    path('page3/', views.page3, name='page3'),
+    path('page4/', views.page4, name='page4')
+]
--- mysite/mymain/views.py
+++ mysite/mymain/views.py
@@ -1,9 +1,19 @@
+'''
 from django.http import HttpResponse
-
-
 def index(request):
     return HttpResponse("<h1>Это мой первый проект на Django</h1>")
-
-
 def new(request):
     return HttpResponse("<h1>Это вторая страница моего проекта на Django</h1>")
+'''
+from django.shortcuts import render
+def page1(request):
+    return render(request, 'mymain/page1.html')
+
+def page2(request):
+    return render(request, 'mymain/page2.html')
+
+def page3(request):
+    return render(request, 'mymain/page3.html')
+
+def page4(request):
+    return render(request, 'mymain/page4.html')
