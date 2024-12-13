82d3a82f06528f9818fc4d685a79173442429a55
Natali280853 <dgorby2000@gmail.com>
2024-12-13 21:13:19 +0500

    del comm

--- mysite/mymain/views.py
+++ mysite/mymain/views.py
@@ -1,10 +1,3 @@
-'''
-from django.http import HttpResponse
-def index(request):
-    return HttpResponse("<h1>Это мой первый проект на Django</h1>")
-def new(request):
-    return HttpResponse("<h1>Это вторая страница моего проекта на Django</h1>")
-'''
 from django.shortcuts import render
 def page1(request):
     return render(request, 'mymain/page1.html')
